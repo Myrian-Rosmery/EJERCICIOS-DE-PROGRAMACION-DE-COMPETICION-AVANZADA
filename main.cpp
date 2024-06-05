90505fe272206f2df4e3521e8568a602c776967f
Myrian-Rosmery <86834396+Myrian-Rosmery@users.noreply.github.com>
2024-06-05 18:43:53 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,35 @@
 #include <iostream>
 #include <vector>
 
-void imprimirSubconjunto(const std::vector<int>& subconjunto) {
+void imprimirSubconjunto(const std::vector<int>& subconjunto, int size) {
     std::cout << "{ ";
-    for (int num : subconjunto) {
-        std::cout << num << " ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << subconjunto[i] << " ";
     }
     std::cout << "}" << std::endl;
 }
-
-void generarSubconjuntos(const std::vector<int>& arr, std::vector<int>& subconjunto, int index, int& totalSubconjuntos) {
-    
-    imprimirSubconjunto(subconjunto);
-    
+void generarSubconjuntos(const std::vector<int>& arr, std::vector<int>& subconjunto, int index, int currentSize, int& totalSubconjuntos) {
+    imprimirSubconjunto(subconjunto, currentSize);
     totalSubconjuntos++;
 
-    
     for (int i = index; i < arr.size(); ++i) {
-        
-        subconjunto.push_back(arr[i]);
 
-        generarSubconjuntos(arr, subconjunto, i + 1, totalSubconjuntos);
+        subconjunto[currentSize] = arr[i];
 
-        subconjunto.pop_back();
+        generarSubconjuntos(arr, subconjunto, i + 1, currentSize + 1, totalSubconjuntos);
     }
 }
 
 
 void generarTodosLosSubconjuntos(const std::vector<int>& arr) {
-    std::vector<int> subconjunto;
+    std::vector<int> subconjunto(arr.size());
     int totalSubconjuntos = 0;
-    generarSubconjuntos(arr, subconjunto, 0, totalSubconjuntos);
+    generarSubconjuntos(arr, subconjunto, 0, 0, totalSubconjuntos);
     std::cout << "Total de subconjuntos: " << totalSubconjuntos << std::endl;
 }
 
 int main() {
-    std::vector<int> arr = {1, 2, 3,4,7,8}; 
+    std::vector<int> arr = {1, 2, 3,4}; // Ejemplo de arreglo
     std::cout << "Todos los subconjuntos posibles:" << std::endl;
     generarTodosLosSubconjuntos(arr);
     return 0;
